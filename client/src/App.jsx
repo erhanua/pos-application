252960a6f78815ad4099131e2c18c8944b1ff9d3
erhanua <106486822+erhanua@users.noreply.github.com>
2023-11-13 22:07:31 +0000

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,15 @@
-import Header from "./components/Header/Header";
-import CartTotals from "./components/cart/CartTotals";
-import Categories from "./components/categories/Categories";
-import Products from "./components/products/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <>
-      <Header />
-      {/* HomePage */}
-      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
-        <div className="categories  overflow-auto max-h-[calc(100vh_-_112px)] md:pb-64">
-          <Categories />
-        </div>
-        <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
-          <Products />
-        </div>
-        <div className="cart-wrapper min-w-[300px]  md:-mr-[24px] md:-mt-[24px] border">
-          <CartTotals />
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/cart/CreateBill.jsx
@@ -0,0 +1,76 @@
+import { Button, Card, Form, Input, Modal, Select } from "antd";
+
+const CreateBill = ({ isModalOpen, setIsModalOpen }) => {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <Modal
+      title="Create Bill"
+      open={isModalOpen}
+      footer={false}
+      onCancel={() => setIsModalOpen(false)}
+    >
+      <Form layout={"vertical"} onFinish={onFinish}>
+        <Form.Item
+          label="Customer Name"
+          name="Customer Name"
+          rules={[
+            {
+              required: true,
+              message: "Customer Name is required",
+            },
+          ]}
+        >
+          <Input placeholder="Write a Customer Name " />
+        </Form.Item>
+        <Form.Item
+          rules={[{ required: true }]}
+          name={"Telephone Number"}
+          label="Telephone Number"
+        >
+          <Input placeholder="Write a Phone Number" maxLength={11} />
+        </Form.Item>
+        <Form.Item
+          label="Payment Method"
+          rules={[{ required: true }]}
+          name={"PaymentMethod"}
+        >
+          <Select placeholder="Select a Payment Method">
+            <Select.Option value="Cash">Cash</Select.Option>
+            <Select.Option value="Credit/Debit Cards">
+              Credit/Debit Cards
+            </Select.Option>
+          </Select>
+        </Form.Item>
+        <Card>
+          <div className="flex justify-between">
+            <span>Sub Total</span>
+            <span>£ 55.80</span>
+          </div>
+          <div className="flex justify-between my-2">
+            <span>Tax</span>
+            <span className="text-red-600">+£ 7.80</span>
+          </div>
+          <div className="flex justify-between">
+            <b>Total</b>
+            <b>£ 55.80</b>
+          </div>
+          <div className="flex justify-end">
+            <Button
+              className="mt-4"
+              type="primary"
+              onClick={() => setIsModalOpen(true)}
+              htmlType="submit"
+            >
+              Create Order
+            </Button>
+          </div>
+        </Card>
+      </Form>
+    </Modal>
+  );
+};
+
+export default CreateBill;
